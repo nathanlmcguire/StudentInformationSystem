de4e1c91beb78cd0749cb8d7099703ff1d4edf24
chris.falkenberg <chris.falkenberg@MU-C3-17.mullen.pvt.k12.co.us>
2014-10-21 12:36:20 -0600

    Constructor is finished and lots of progress on submenus

--- StudentInformationSystem/src/Student.java
+++ StudentInformationSystem/src/Student.java
@@ -24,10 +24,71 @@ public class Student
 		periodThree = p3;
 		periodThreeGrade = p3g;
 		}
-	
-	/*
-	 * make getters and setters
-	 */
+
+	public String getFirstName()
+		{
+		return firstName;
+		}
+	public void setFirstName(String firstName)
+		{
+		this.firstName = firstName;
+		}
+	public String getLastName()
+		{
+		return lastName;
+		}
+	public void setLastName(String lastName)
+		{
+		this.lastName = lastName;
+		}
+	public String getPeriodOne()
+		{
+		return periodOne;
+		}
+	public void setPeriodOne(String periodOne)
+		{
+		this.periodOne = periodOne;
+		}
+	public String getPeriodOneGrade()
+		{
+		return periodOneGrade;
+		}
+	public void setPeriodOneGrade(String periodOneGrade)
+		{
+		this.periodOneGrade = periodOneGrade;
+		}
+	public String getPeriodTwo()
+		{
+		return periodTwo;
+		}
+	public void setPeriodTwo(String periodTwo)
+		{
+		this.periodTwo = periodTwo;
+		}
+	public String getPeriodTwoGrade()
+		{
+		return periodTwoGrade;
+		}
+	public void setPeriodTwoGrade(String periodTwoGrade)
+		{
+		this.periodTwoGrade = periodTwoGrade;
+		}
+	public String getPeriodThree()
+		{
+		return periodThree;
+		}
+	public void setPeriodThree(String periodThree)
+		{
+		this.periodThree = periodThree;
+		}
+	public String getPeriodThreeGrade()
+		{
+		return periodThreeGrade;
+		}
+	public void setPeriodThreeGrade(String periodThreeGrade)
+		{
+		this.periodThreeGrade = periodThreeGrade;
+		}
 	}
 
 
