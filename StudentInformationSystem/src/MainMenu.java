2feb01fcb354db06dd58c6b657a8768a945164a0
berrysa@mullenmail.com <berrysa@mullenmail.com>
2014-10-21 19:27:18 -0600

    main menu is almost finished

--- StudentInformationSystem/src/MainMenu.java
+++ StudentInformationSystem/src/MainMenu.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MainMenu
 	{
-	static String option1, addOrDelete, change;
+	static String option1, addOrDelete, change, sort;
 	public static void main(String[] args)
 		{
 		Firstquestion();
@@ -20,13 +20,47 @@ public class MainMenu
 			System.out.println("Would you prefer to: \n 1) Add a student \n 2) Delete a student");
 			Scanner userInput2 = new Scanner(System.in);
 			addOrDelete = userInput2.nextLine();
+			if(addOrDelete.equals("1"))
+			{
+				//Chris' method to add a student
+			}
+			if(addOrDelete.equals("2"))
+			{
+				//Chris' method to delete a student
+			}
 			}
 		if(option1.equals("2"))
 			{
 			System.out.println("Would you prefer to: \n 1) Change a student's schedule \n 2) Change a student's grade");
 			Scanner userInput3 = new Scanner(System.in);
 			change = userInput1.nextLine();
+			if(change.equals(1))
+			{
+				//Nathan's method to change a students schedule
 			}
+			if(change.equals(2))
+			{
+				//Nathan's method to change a students grade
+			}
+			}
+		if(option1.equals("3"))
+		{
+			System.out.println("Would you prefer to sort the students by: \n 1) Last Name \n 2) GPA \n 3) Class");
+			Scanner userInput3 = new Scanner(System.in);
+			sort = userInput1.nextLine();
+			if(sort.equals("1"))
+			{
+				//SortByLastName
+			}
+			if(sort.equals("2"))
+			{
+				//SortByGPA
+			}
+			if(sort.equals("3"))
+			{
+				//SortByClass
+			}
+		}
 		}
 
 	public static void Thirdquestion()
