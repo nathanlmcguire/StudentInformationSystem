285ebbd278c4de45c763e7328a2844c4042336b3
samantha.berry <samantha.berry@MU-C3-19.mullen.pvt.k12.co.us>
2014-10-23 13:41:15 -0600

    put in while loop

--- StudentInformationSystem/src/SortByClass.java
+++ StudentInformationSystem/src/SortByClass.java
@@ -4,11 +4,7 @@ public class SortByClass implements Comparator<Student>
 	{
 	public int compare (Student s1, Student s2)
 		{
-		if(s1.getPeriodOne() 
-				s2.getPeriodOne()  )
-			return 1;
-		else
-			return -1;
+		return s1.getPeriodOne().compareTo(s2.getPeriodOne());
 		}
 
 		}
